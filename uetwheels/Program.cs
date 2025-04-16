b725f923619d7cbc0c487b6d64cf4263a157536f
zainabzahid1905 <zainabzahid288@gmail.com>
2025-04-16 13:50:58 +0500

    updated

--- uetwheels/Program.cs
+++ uetwheels/Program.cs
@@ -17,6 +17,7 @@ namespace uetwheels
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("hello world");
         }
     }
 }
